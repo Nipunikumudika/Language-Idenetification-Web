491eec3f53ba9a24f9e997bbd04ba58d6da5fb5e
Nipuni <nipunikumudika@gmail.com>
2024-03-02 17:50:05 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
 
 app.use(cors());
 app.use(bodyParser.text()); 
@@ -34,4 +38,4 @@ app.post('/predict', (req, res) => {
   });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
